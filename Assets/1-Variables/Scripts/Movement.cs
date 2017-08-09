d9bd33e694b847251bfa90ee43f7daed4acc3630
Duane Reymond <duane.reymond@gmail.com>
2017-08-09 12:25:30 +1000

    - Testing Commit>Sync>Push.

---
+++ Assets/1-Variables/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clean Code: CTRL+K+D
+
+namespace Variables
+{ 
+    public class Movement : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+        public float rotationSpeed = 200f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Get Horizontal Input
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+
+            // Direction (variable) x Movement Speed (variable) x DeltaTime (constant)
+            // transform.Translate(new Vector3(inputH, inputV) * movementSpeed * Time.deltaTime);
+            // transform.position += new Vector3(inputH, inputV) * movementSpeed * Time.deltaTime;
+            // Creating new Vector3 Function for this transform line
+            // Using Inputs, multiplied by movementSpeed then multiplied by deltaTime (which is CPU speed!)
+
+            // Direction x Input(sign) x Speed x DeltaTime
+            transform.position += transform.right * inputV * movementSpeed * Time.deltaTime; // Forward goes Right
+            // transform.position += -transform.up * inputH * movementSpeed * Time.deltaTime;
+
+            // Velocity (i.e. Direction x Rotation Speed) x DeltaTime
+            // transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
+            // transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+            // another e.g.: transform.eulerAngles += Vector3.forward * rotationSpeed * Time.deltaTime;
+
+            transform.eulerAngles += Vector3.back * inputH * rotationSpeed * Time.deltaTime; // -Vector3.forward?
+
+            // if(inputH !=0) // if InputH NOT EQUAL to 0
+            // {
+            //    Debug.Log(inputH); // show values in DebugLog
+            // }
+            // if(inputV !=0)
+            // {
+            //    Debug.Log(inputV);
+            // }
+        }
+    }
+}
---
+++ Assets/1-Variables/Scripts/Rigid2DMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CTRL+K+D (Clean Up Code)
+
+// Namespace / Class / Function
+
+namespace Variables
+{
+
+    public class Rigid2DMovement : MonoBehaviour
+    {
+
+        // Creating Variables
+        public float movementSpeed = 20f;
+        public float rotationSpeed = 20f;
+        public float deceleration = 0.1f;
+
+        private Rigidbody2D rigid2D;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Get component from GameObject
+            rigid2D = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame; Update can be used to check Movement
+        void Update()
+        {
+            // Call Movement and Rotation Functions so Unity knows about it!
+            Movement();
+            Rotation();
+        }
+
+        // Void creates Functions. Do NOT put Functions inside other Functions. No Order of Functions within the Class. Order matters within a Function.
+        void Movement()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            rigid2D.AddForce(transform.right * inputV * movementSpeed);
+            rigid2D.velocity += -rigid2D.velocity * deceleration * Time.deltaTime; // Velocity = Direction & Speed
+        }
+
+        void Rotation()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.back * inputH * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
