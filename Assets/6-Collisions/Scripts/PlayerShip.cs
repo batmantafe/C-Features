043c8db385db8bd566011407923f75ee5fb61f02
Duane Reymond <duane.reymond@gmail.com>
2017-08-17 11:55:12 +1000

    17/8 -Added 6-Collisions. -Added Billiards.

---
+++ Assets/6-Collisions/Scripts/PlayerShip.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collisions
+{
+    public class PlayerShip : MonoBehaviour
+    {
+
+        //[RequireComponent(typeof(Rigidbody2D))]
+
+        public float acceleration = 50f;
+        public float rotationSpeed = 5f;
+
+        private Rigidbody2D rigid;
+
+        // Use this for initialization
+        void Start()
+
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Accelerate();
+            Rotate();
+        }
+
+        void Accelerate()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            rigid.AddForce(transform.up * inputV * acceleration);
+        }
+
+        void Rotate()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.back, rotationSpeed * inputH);
+        }
+    }
+}
