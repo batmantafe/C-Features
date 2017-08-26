29de0b5b8c5074a7079cf0666f5dfd536b75ec6c
Duane Reymond <duane.reymond@gmail.com>
2017-08-26 10:33:03 +1000

    26/8 -Added Breakout (Arrays 2) from Google Drive (Paddle and Ball). -However, Paddle not firing Balls?!

---
+++ Assets/~Breakout/Scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+
+    public class Ball : MonoBehaviour
+    {
+        public float speed = 5f; // Speed that the ball travels
+
+        private Vector3 velocity; // Velocity of the ball (Direction X Speed)
+        
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Moves ball using velocity & deltaTime
+            transform.position += velocity * Time.deltaTime;
+        }
+
+        // Fires off ball in a given direction (sends the ball flying in a given direction)
+        public void Fire(Vector3 direction)
+        {
+            // Calculate velocity
+            velocity = direction * speed;
+        }
+
+        // Detect collisions (reflects ball depending on collision)
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            // Grab contact point of collision
+            ContactPoint2D contact = other.contacts[0];
+
+            // Calculate the reflection point of the ball using the velocity & contact normal
+            Vector3 reflect = Vector3.Reflect(velocity, contact.normal);
+
+            // Calculate new velocity from reflection multiply by the same speed (velocity.magnitude)
+            velocity = reflect.normalized * velocity.magnitude;
+        }
+
+        
+
+    }
+}
