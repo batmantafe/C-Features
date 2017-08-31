1996550edf14e5a2b5189e0e8253dc5d3a4bd1e3
Duane Reymond <duane.reymond@gmail.com>
2017-08-31 11:51:15 +1000

    31/8: -TowerDefense added. Navmesh baked. AIAgent script done.

---
+++ Assets/~TowerDefense/Scripts/AIAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TowerDefense
+{
+    
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AIAgent : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent nav;
+
+        // Use this for initialization
+
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if target is != null
+            if (target != null)
+            {
+                // set destination to target's position
+                nav.SetDestination(target.position);
+            }
+
+
+        }
+    }
+}
