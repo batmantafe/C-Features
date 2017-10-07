c1b6823fa0c5d8ac2b7d6d428e33a3157f8376bc
Duane Reymond <duane.reymond@gmail.com>
2017-10-07 14:56:49 +1100

    -A Hundred Balls lesson complete. -Still need to do Homework Tasks 1 & 2 for it.

---
+++ Assets/~AHundredBalls/Scripts/PaddleScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>(); // Get the Animator component
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.DownArrow)) // Check if Down Arrow pressed
+        {
+            // Modify parameter we created earlier
+            anim.SetBool("isOpened", true);
+        }
+
+        else // If the Down Arrow isn't pressed
+        {
+            // Set that parameter to false
+            anim.SetBool("isOpened", false);
+        }
+	}
+}
