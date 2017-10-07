2dc871cbb686ec9d4006f29685356bca3d2b985e
Duane Reymond <duane.reymond@gmail.com>
2017-10-07 17:36:01 +1100

    -A Hundred Balls: Homework Task 1 done. Trail Renderer added.

--- Assets/~AHundredBalls/Scripts/BallMaterialScript.cs
+++ Assets/~AHundredBalls/Scripts/BallMaterialScript.cs
@@ -9,19 +9,44 @@ namespace AHundredBalls
     {
         public Material[] mats = null;
         public Renderer rend;
+        public TrailRenderer trail;
 
         // Use this for initialization
         void Start()
         {
-            int randomMatIndex = Random.Range(0, mats.Length);
-            rend = GetComponent<Renderer>();
-            rend.material = mats[randomMatIndex];
+            RandomMat();
+
+            RandomTrail();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        void RandomMat()
+        {
+            int randomMatIndex = Random.Range(0, mats.Length);
+            rend = GetComponent<Renderer>();
+            rend.material = mats[randomMatIndex];
+        }
+
+        void RandomTrail()
+        {
+            trail = gameObject.GetComponent<TrailRenderer>();
+
+            int randomNumber = Random.Range(0, 4); // min Incl, max Excl
+
+            if (randomNumber == 3)
+            {
+                trail.enabled = true;
+            }
+
+            if (randomNumber <= 2)
+            {
+                trail.enabled = false;
+            }
         }
     }
 }
