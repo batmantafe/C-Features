827679d149d93f29895772376e58d3e86f339db4
batmantafe <duane.reymond@gmail.com>
2017-10-10 16:05:43 +1100

    Replaced with new 8-Inheritance.

---
+++ Assets/8-Inheritance/Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; // For AI!
+
+namespace Inheritance
+{
+    public class Enemy : MonoBehaviour
+    {
+        [Header("Enemy")]
+        public Transform target;
+        public int health = 100;
+        public int damage = 20;
+        public float attackRange = 2f;
+        public float attackRate = .5f;
+
+        private float attackTimer = 0f;
+        private NavMeshAgent nav;
+        private Rigidbody rigid;
+        
+
+        
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            // Increase attackTimer
+            attackTimer += Time.deltaTime;
+
+            // Get distance from Enemy to Target
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            // IF distance < attackRange
+            if(distance < attackRange)
+            {
+                // Call Attack()
+                Attack();
+
+                // Reset attackTimer
+                attackTimer = 0f;
+            }
+
+            // IF Target != null
+            if(target != null)
+            {
+                // Navigate to Target
+                nav.SetDestination(target.position);
+            }
+
+        }
+
+        public virtual void Attack() // Virtual is replaced by Override
+        {
+            print("Attack() called");
+        }
+    }
+}
