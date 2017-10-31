8e46171ddff3cf5de1a00f00e04d4dbbd4d95104
Duane Reymond <duane.reymond@gmail.com>
2017-10-31 16:13:23 +1100

    -Added New Asteroids.

---
+++ Assets/~Asteroids/Scripts/Moving.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Moving : MonoBehaviour
+    {
+        public float acceleration = 50f;
+        public float rotationSpeed = 360f;
+
+        private Rigidbody2D rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Accelerates the Player
+        void Accelerate()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            rigid.AddForce(transform.up * inputV * acceleration);
+        }
+
+        // Rotates the Player (in degrees)
+        void Rotate()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.back, rotationSpeed * inputH * Time.deltaTime);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Accelerate();
+            Rotate();
+        }
+    }
+}
